e5335a7a0fc30619f8c8abceeaff2f14163ed28d
Timur Kabaev <timur_kabaev@epam.com>
2022-04-17 12:10:41 +0300

    Initial commit

---
+++ src/main/java/com/ltsapp/fileanalyzer/fileanalyzerservice/FileAnalyzerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ltsapp.fileanalyzer.fileanalyzerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAnalyzerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAnalyzerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ltsapp/fileanalyzer/fileanalyzerservice/FileAnalyzerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltsapp.fileanalyzer.fileanalyzerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAnalyzerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
